d3adce25ae91e3922e097daa9dc1b1db84c96df6
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-31 17:33:32 +0600

    feat: implement Logo component and integrate it into the Navbar layout

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,7 +2,27 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-export const Logo = ({ className }: { className?: string }) => {
+export const Logo = ({ className, forceWhite = false }: { className?: string; forceWhite?: boolean }) => {
+  if (forceWhite) {
+    return (
+      <Link href="/" className="inline-block cursor-pointer">
+        <div className="">
+          <Image
+            src="/apexLogo.png"
+            width={180}
+            height={40}
+            loading="eager"
+            className={cn(
+              "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain",
+              className
+            )}
+            alt={`${process.env.NEXT_PUBLIC_APP_NAME}`}
+          />
+        </div>
+      </Link>
+    );
+  }
+
   return (
     <Link href="/" className="inline-block cursor-pointer">
       <div className="">
@@ -12,7 +32,7 @@ export const Logo = ({ className }: { className?: string }) => {
           height={40}
           loading="eager"
           className={cn(
-            "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain dark:hidden",
+            "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain dark:hidden invert",
             className
           )}
           alt={`${process.env.NEXT_PUBLIC_APP_NAME}`}
