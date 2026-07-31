93edebc86fd44d7abb6187bc660fdfbf972a5174
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-31 16:28:42 +0600

    feat: add responsive Logo component with theme-aware image variants

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,31 +1,34 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = ({ className }: { className?: string }) => {
   return (
-    <div className="">
-      <Image
-        src="/apexLogo.png"
-        width={180}
-        height={40}
-        loading="eager"
-        className={cn(
-          "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain dark:hidden",
-          className
-        )}
-        alt={`${process.env.NEXT_PUBLIC_APP_NAME}`}
-      />
-      <Image
-        src="/apexLogo.png"
-        width={180}
-        height={40}
-        loading="eager"
-        className={cn(
-          "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain not-dark:hidden",
-          className
-        )}
-        alt={`${process.env.NEXT_PUBLIC_APP_NAME}`}
-      />
-    </div>
+    <Link href="/" className="inline-block cursor-pointer">
+      <div className="">
+        <Image
+          src="/apexLogo.png"
+          width={180}
+          height={40}
+          loading="eager"
+          className={cn(
+            "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain dark:hidden",
+            className
+          )}
+          alt={`${process.env.NEXT_PUBLIC_APP_NAME}`}
+        />
+        <Image
+          src="/apexLogo.png"
+          width={180}
+          height={40}
+          loading="eager"
+          className={cn(
+            "xl:w-[170px] md:w-[140px] xs:w-[120px] w-[100px] h-auto lg:h-[55px] object-contain not-dark:hidden",
+            className
+          )}
+          alt={`${process.env.NEXT_PUBLIC_APP_NAME}`}
+        />
+      </div>
+    </Link>
   );
 };
