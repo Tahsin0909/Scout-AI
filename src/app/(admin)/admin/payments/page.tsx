1a5303fcff2bebd2090acfd48585323be0edd814
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-31 15:42:37 +0600

    payment route implement is done

---
+++ src/app/(admin)/admin/payments/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PaymentManagement from "@/components/dashboard/payments/PaymentManagement";
+
+const page = () => {
+    return (
+        <PaymentManagement />
+    );
+};
+
+export default page;
