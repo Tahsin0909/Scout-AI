d63dd9b016d666e262f23c6d4dacd1ae0f561531
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-31 12:44:33 +0600

    testimonial page is updated

--- src/components/testimonials/TestimonialsSection.tsx
+++ src/components/testimonials/TestimonialsSection.tsx
@@ -177,16 +177,17 @@ export default function TestimonialsSection() {
         <section className="overflow-hidden bg-background py-20 sm:py-24 lg:py-[110px] relative">
             {/* Header */}
             <div className="mb-12 text-center md:mb-16 max-w-3xl mx-auto">
-                <p className="mb-4 text-sm font-medium text-muted-foreground uppercase tracking-wide border w-fit mx-auto px-3 py-1 rounded-full bg-white/5 ">
+                {/* <p className="mb-4 text-sm font-medium text-muted-foreground uppercase tracking-wide border w-fit mx-auto px-3 py-1 rounded-full bg-white/5 ">
                     From Our Adventurers
-                </p>
+                </p> */}
                 <h2 className="mb-4 text-3xl font-bold tracking-tight sm:text-4xl lg:text-5xl">
                     What People Are{' '}
                     <span className="text-[#97B900] italic">Saying</span>
                 </h2>
                 <p className="mx-auto max-w-2xl text-base text-muted-foreground sm:text-lg">
-                    See what reviewers, explorers, and outdoor professionals
-                    are saying about Apex Adventure Lab
+                    {/* See what reviewers, explorers, and outdoor professionals
+                    are saying about Apex Adventure Lab */}
+                    See what reviewers are saying about TripTrax.
                 </p>
             </div>
 
