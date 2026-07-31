42c9a7b10193503c7ce3fae53db554b74e456440
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-31 15:58:40 +0600

    feat: implement admin content management dashboard with search and pagination features

---
+++ src/app/(admin)/admin/content/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContentManagement from "@/components/dashboard/content/ContentManagement";
+
+const page = () => {
+    return (
+        <ContentManagement />
+    );
+};
+
+export default page;
