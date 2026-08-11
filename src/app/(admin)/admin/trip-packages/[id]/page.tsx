25e052e140cb20e5e2db25cfe378dd14d78bd422
Tahsin0909 <tajisan789@gmail.com>
2026-08-11 11:27:55 +0600

    updated admin trp report and working on user profile

--- src/app/(admin)/admin/trip-packages/[id]/page.tsx
+++ src/app/(admin)/admin/trip-packages/[id]/page.tsx
@@ -1,9 +1,11 @@
 import UnderConstruction from '@/components/others-state/underConstruction';
+import TripReport from '@/features/triptrax/components/tripReport/TripReport';
 
 const page = () => {
     return (
         <div>
-            <UnderConstruction name='Trip Details' type='server' />
+            <UnderConstruction name='Trip Details' type='server' description='The Page design is completed Admin will be able to edit it so waiting for the server after that it will be edit able' />
+            <TripReport />
         </div>
     );
 };
