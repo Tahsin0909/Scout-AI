b8a8a17d2d00ce2e1674aac921f5a672b6a346b2
Tahsin0909 <tajisan789@gmail.com>
2026-08-19 14:26:14 +0600

    update the manifest for app icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,13 +17,13 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: '/apexLogo-192.png',
                 sizes: '192x192',
                 type: 'image/png',
-                purpose: 'any',
+                purpose: 'maskable',
             },
             {
                 src: '/apexLogo-512.png',
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: 'any',
+                purpose: 'maskable',
             },
         ],
     }
